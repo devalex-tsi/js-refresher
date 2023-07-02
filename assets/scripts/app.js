d22aa7d6b5aee647d4859ece9a9e1367dcb26439
Alex Tsi <alex.tsi.dev@gmail.com>
2023-07-02 14:39:19 +0200

    functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,3 +4,28 @@
 // console.log(myVar.default);
 // console.log(myVar.apiKey);
 // console.log(myVar.someOther);
+
+
+// const myVar = 10;
+// const myVar = 10
+//
+// function getSum(a, b = 0) {
+//     return a + b;
+// }
+//
+// const sum1 = getSum(10)
+// const sum2 = getSum(10, 5)
+//
+// console.log(sum1)
+// console.log(sum2)
+
+//arrow functions
+
+const arrowFunc = () => {
+    console.log('works')
+}
+const arrowFunc2 = () => console.log('works')
+
+arrowFunc()
+
+
