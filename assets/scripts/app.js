f5a79f4c97426f10d06ef9a0e08c6a7d339e5f6a
Alex Tsi <alex.tsi.dev@gmail.com>
2023-07-02 13:26:37 +0200

    init

---
+++ assets/scripts/app.js
@@ -0,0 +1,6 @@
+//import myVar from "./util.js";
+// import * as myVar from "./util.js";
+//
+// console.log(myVar.default);
+// console.log(myVar.apiKey);
+// console.log(myVar.someOther);
---
+++ assets/scripts/util.js
@@ -0,0 +1,4 @@
+export default '123qwerty'
+
+export let apiKey = 'serverAPI key'
+export let someOther = 'other exported value'
