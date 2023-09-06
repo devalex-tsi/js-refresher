4b134e1013e176032a8d31ac969bbe2048c1e2fd
Alex Tsi <alex.tsi.dev@gmail.com>
2023-09-06 19:16:44 +0200

    the dominant property is the property described inside the type

--- assets/scripts/classes.js
+++ assets/scripts/classes.js
@@ -1,6 +1,9 @@
 function Animal(type, age) {
     this.type = type
     this.age = age
+
+    //the dominant property is the property described inside the type
+    this.mammalsSubClass = 'warm-blooded'
 }
 
 const mammals1 = new Animal('mammals', 5)
@@ -11,7 +14,12 @@ Animal.prototype.eat = function () {
     console.log(`${this.type} animal is eating`)
 }
 
+Animal.prototype.mammalsSubClass = 'vertebrate animals'
+
 mammals1.eat()
 mammals2.eat()
 
-console.log(mammals1.eat === mammals2.eat)//?
+console.log(mammals1.eat === mammals2.eat)
+
+//!the dominant property is the property described inside the type
+console.log(mammals1.mammalsSubClass)
